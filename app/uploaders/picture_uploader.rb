2a27b444ff2c60c5144a93e9708d68ae098e684d
Takahiro Kobayashi <kbys1025@gmail.com>
2018-12-19 21:45:47 +0900

    delete exif info

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,7 +1,8 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
-  
+  process :strip
+
   storage :file
 
   # アップロードファイルの保存先ディレクトリは上書き可能
@@ -13,5 +14,14 @@ class PictureUploader < CarrierWave::Uploader::Base
   # アップロード可能な拡張子のリスト
   def extension_whitelist
     %w(jpg jpeg gif png)
+  end
+
+  # exif情報を削除して画像をアップロードできるようにする
+  def strip
+    manipulate! do |img|
+      img.strip
+      img = yield(img) if block_given?
+      img
+    end
   end
 end
